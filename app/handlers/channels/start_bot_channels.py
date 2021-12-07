bc34859650152a59979cd63cc5f5330ec8d5c5f1
Kirill Vashchenko <k.vashhenko@gmail.com>
2021-12-07 15:41:04 +0300

    fix code + add exception BotKicked

--- app/handlers/channels/start_bot_channels.py
+++ app/handlers/channels/start_bot_channels.py
@@ -1,6 +1,7 @@
 from aiogram import Dispatcher, Bot
 from aiogram.dispatcher import FSMContext
 from aiogram.types import Message, ContentTypes
+from aiogram.utils.exceptions import BotKicked
 from odmantic import AIOEngine
 
 from app.keyboards.inline import ChannelUserMarkup
@@ -11,7 +12,7 @@ from app.utils.valid.valid_admins_channel import valid_admin_in_channels
 
 
 async def add_channels(m: Message, _: i18n):
-    await m.answer(_('Добавьте своего бота'))
+    await m.answer(_('Добавьте своей канал, (просто перешлите любую публикацию из вашего канала)'))
     await AddChannels.user_channels.set()
 
 
@@ -23,12 +24,12 @@ async def add_channels_user(m: Message, db: AIOEngine, bot: Bot, user: UserModel
         user_id = m.from_user.id
         valid = await valid_admin_in_channels(m, admins_chat, user_id, _)
         if valid:
-            await m.answer(_('Проверка пройдена'))
+            await m.answer(_('Канал добавлен'))
             user.channels.append(channel)
             await db.save(user)
         else:
-            await m.answer(_('Проверка не пройдена'))
-    except:
+            await m.answer(_('Вы не являетесь администратором канала'))
+    except BotKicked:
         await m.answer(_('Бот не имеет доступа к каналу'))
         await state.finish()
 
@@ -39,6 +40,6 @@ async def user_channels(m: Message, user: UserModel, bot: Bot, _: i18n):
 
 
 def setup(dp: Dispatcher):
-    dp.register_message_handler(add_channels, commands='add_bot')
+    dp.register_message_handler(add_channels, commands='add_channel')
     dp.register_message_handler(add_channels_user, state=AddChannels.user_channels, content_types=ContentTypes.ANY)
     dp.register_message_handler(user_channels, commands='my_channels')
