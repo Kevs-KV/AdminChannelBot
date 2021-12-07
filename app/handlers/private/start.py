34192e0816bd6ce1c049cc56910dafc167dbffb9
Kirill Vashchenko <k.vashhenko@gmail.com>
2021-12-07 15:04:12 +0300

    fix code

--- app/handlers/private/start.py
+++ app/handlers/private/start.py
@@ -1,12 +1,11 @@
+import pytz
 from aiogram import Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.types import Message, CallbackQuery
-
-from app.keyboards.inline import ChoiceLanguageUser
-from app.middlewares import i18n
-from app.models import UserModel
 from odmantic import AIOEngine
 
+from app.keyboards.inline import ChoiceLanguageUser
+from app.models import UserModel
 from app.states.language_states import LanguageUser
 
 
@@ -16,11 +15,12 @@ async def get_start_message(message: Message):
         "🇷🇺 Выберите язык:\n\n" "🇬🇧 Choose your language:",
         reply_markup=markup,
     )
+    print(pytz.all_timezones)
     await LanguageUser.start.set()
 
 
 async def set_user_language(
-    query: CallbackQuery, db: AIOEngine, callback_data: dict, user: UserModel, state: FSMContext
+        query: CallbackQuery, db: AIOEngine, callback_data: dict, user: UserModel, state: FSMContext
 ):
     await query.answer()
     language = callback_data.get("language")
@@ -30,13 +30,13 @@ async def set_user_language(
     i18n.ctx_locale.set(language)
     await query.message.edit_text(
         _(
-            "Привет! Данный бот предназначен для администрования телеграм каналов, для списка команды введите - /help"
+            "Привет! Данный бот предназначен для администрования телеграм каналов, для списка команды введите - /help",
         )
     )
     await state.finish()
 
 
-
 def setup(dp: Dispatcher):
     dp.register_message_handler(get_start_message, commands="start")
-    dp.register_callback_query_handler(set_user_language, ChoiceLanguageUser.callback_data.filter(), state=LanguageUser.start)
+    dp.register_callback_query_handler(set_user_language, ChoiceLanguageUser.callback_data.filter(),
+                                       state=LanguageUser.start)
