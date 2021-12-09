ac30425a16b7e06e02c1637ec0533cf81a072e31
Kirill Vashchenko <k.vashhenko@gmail.com>
2021-12-09 16:16:43 +0300

    del showsmodel

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,10 +1,8 @@
 from .chat import ChatModel
 from .user import UserModel, UserRoles
-from .shows import ShowsModel
 
 __all__ = (
     "ChatModel",
     "UserModel",
     "UserRoles",
-    "ShowsModel"
 )
