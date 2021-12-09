df867e8e18921f21d8f5030986e27b005438c6b6
Kirill Vashchenko <k.vashhenko@gmail.com>
2021-12-09 16:23:32 +0300

    fix cancel

--- app/handlers/channels/__init__.py
+++ app/handlers/channels/__init__.py
@@ -1,8 +1,8 @@
 from aiogram import Dispatcher
 
-from app.handlers.channels import new_post, start_bot_channels, poster, scheduler_control
+from app.handlers.channels import cancel, new_post, start_bot_channels, poster, scheduler_control
 
 
 def setup(dp: Dispatcher):
-    for module in (new_post, start_bot_channels, poster, scheduler_control):
+    for module in (cancel, new_post, start_bot_channels, poster, scheduler_control):
         module.setup(dp)
