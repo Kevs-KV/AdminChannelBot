61d1188f99e38eb2b5fc05cb4d76ddda2540f202
Kirill Vashchenko <k.vashhenko@gmail.com>
2021-12-09 16:06:47 +0300

    add setup + fix code

--- app/handlers/channels/cancel.py
+++ app/handlers/channels/cancel.py
@@ -2,6 +2,13 @@ from aiogram import Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.types import Message
 
+from app.middlewares import i18n
 
-async def user_commands_cancel(m: Message, state: FSMContext):
+
+async def user_command_cancel_state(m: Message, state: FSMContext, _: i18n):
     await state.finish()
+    await m.answer(_('Отменено'))
+
+
+def setup(dp: Dispatcher):
+    dp.register_message_handler(user_command_cancel_state, state='*')
