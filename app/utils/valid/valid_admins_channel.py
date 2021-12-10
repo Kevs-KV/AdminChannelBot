c03f8b0568590d3e79bde08f7c3e6b41481b14d3
Kirill Vashchenko <k.vashhenko@gmail.com>
2021-12-10 14:29:21 +0300

    add valid time posting

--- app/utils/valid/valid_admins_channel.py
+++ app/utils/valid/valid_admins_channel.py
@@ -1,5 +1,12 @@
-from aiogram.contrib.middlewares import i18n
+from datetime import datetime
+
+from aiogram import Bot
 from aiogram.types import Message
+from odmantic import AIOEngine
+from pytz import timezone
+
+from app.middlewares import i18n
+from app.models import UserModel
 
 
 async def valid_admin_in_channels(m: Message, admins_chat, user_id, _: i18n):
@@ -7,7 +14,14 @@ async def valid_admin_in_channels(m: Message, admins_chat, user_id, _: i18n):
         if admin['id'] == user_id:
             return True
     else:
-        await m.answer('Вы не администратор канала')
+        await m.answer(_('Вы не администратор канала'))
         return False
 
+
+async def valid_time_posting(bot: Bot, user: UserModel, db: AIOEngine, data_time):
+    hour, minute, day, month, year = data_time
+    user_timezone = timezone(user.timezone)
+    if datetime.now(user_timezone) < user_timezone.localize(datetime(year, month, day, hour, minute)):
+        return True
+    raise ValueError
 
