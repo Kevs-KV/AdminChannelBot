5003c0d479672a2b98d25455caec9316dc04c0e2
Kirill Vashchenko <k.vashhenko@gmail.com>
2021-12-10 08:43:35 +0300

    renaming file

--- app/handlers/channels/__init__.py
+++ app/handlers/channels/__init__.py
@@ -1,8 +1,8 @@
 from aiogram import Dispatcher
 
-from app.handlers.channels import cancel, new_post, start_bot_channels, poster, scheduler_control
+from app.handlers.channels import cancel, new_post, add_channel, poster, scheduler_control
 
 
 def setup(dp: Dispatcher):
-    for module in (cancel, new_post, start_bot_channels, poster, scheduler_control):
+    for module in (cancel, new_post, add_channel, poster, scheduler_control):
         module.setup(dp)
