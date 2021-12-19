fc1feb0a4db2b9afa086235f986021fc696ca14b
Kevs <k.vashhenko@gmai.com>
2021-12-19 13:40:43 +0300

    fix name

--- app/handlers/channels/add_channel.py
+++ app/handlers/channels/add_channel.py
@@ -12,13 +12,13 @@ from app.states.bot_states import AddChannels
 from app.utils.valid.valid_admins_channel import valid_admin_in_channels
 
 
-async def add_channels(m: Message, _: i18n):
+async def add_channel(m: Message, _: i18n):
     await m.answer(_('Добавьте свой канал, (просто перешлите любую публикацию из вашего канала)'),
                    reply_markup=CancelUserAction().get(_))
     await AddChannels.user_channels.set()
 
 
-async def add_channels_user(m: Message, db: AIOEngine, bot: Bot, user: UserModel, state: FSMContext, _: i18n):
+async def add_channel_user(m: Message, db: AIOEngine, bot: Bot, user: UserModel, state: FSMContext, _: i18n):
     try:
         channel = m.forward_from_chat['id']
         admins_object = await bot.get_chat_administrators(chat_id=channel)
@@ -44,12 +44,12 @@ async def add_channels_user(m: Message, db: AIOEngine, bot: Bot, user: UserModel
         await m.answer(_('Перешлите публикацию из канала'), reply_markup=ReplyKeyboardRemove())
 
 
-async def user_channels(m: Message, user: UserModel, bot: Bot, _: i18n):
+async def user_channel(m: Message, user: UserModel, bot: Bot, _: i18n):
     markup = await ChannelUserMarkup(user, bot).get()
     await m.answer(_('Ваши каналы:'), reply_markup=markup)
 
 
 def setup(dp: Dispatcher):
-    dp.register_message_handler(add_channels, commands='add_channel')
-    dp.register_message_handler(add_channels_user, state=AddChannels.user_channels, content_types=ContentTypes.ANY)
-    dp.register_message_handler(user_channels, commands='my_channels')
+    dp.register_message_handler(add_channel, commands='add_channel')
+    dp.register_message_handler(add_channel_user, state=AddChannels.user_channels, content_types=ContentTypes.ANY)
+    dp.register_message_handler(user_channel, commands='my_channels')
